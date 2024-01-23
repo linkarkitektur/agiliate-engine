4bab4795ef07227ff9758c155e6c33650722e6dc
Thomas T <huulbaek@gmail.com>
2024-01-23 11:28:52 +0100

    Fix path in test

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,7 +4,7 @@ import Calculator from '../src/calculations/calculator'
 test("totals", async () => {
   const file = Bun.file('test/scenarios/1.json')
   const scenario = await file.json()
-  const calculator = new Calculator(scenario.input.variables, scenario.input.customSpaceConstants, scenario.input.customConstants, 'v1')
+  const calculator = new Calculator(scenario.input.variables, scenario.input.customSpaceConstants, scenario.input.customConstants, '1')
   const result = calculator.result()
   expect(result.totals.grossArea).toBe(scenario.result.totals.grossArea)
 })
